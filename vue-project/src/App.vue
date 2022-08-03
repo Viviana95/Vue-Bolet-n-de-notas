1a444b71fe16f40e29ef0540aab50a10a0e6ce94
Viviana <vivitr5195@gmail.com>
2022-08-03 17:00:14 +0200

    add report card

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -4,12 +4,12 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
+<nav class="navbar navbar-dark bg-dark navbar-expand-lg ">
+ <RouterLink to="/">Home</RouterLink>
+ <RouterLink to="/report">Report</RouterLink>
+</nav>
 
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
   
  
 
