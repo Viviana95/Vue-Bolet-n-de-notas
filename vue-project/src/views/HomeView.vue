70d0defd00beb4e43dee98c50a377a6c4eb559be
Viviana <vivitr5195@gmail.com>
2022-08-03 15:32:59 +0200

    modify components and views

---
+++ vue-project/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from '../components/HelloWorld.vue';
+
+</script>
+
+<template>
+<button type="button" class="btn btn-primary btn-lg">Boletin</button>
+
+</template>
